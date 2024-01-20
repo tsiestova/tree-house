5dc0e2ab84baf6b58a7c072622b4c8c0d1a7c2e6
Tetiana Siestova <siestova.t@gmail.com>
2024-01-20 14:31:44 +0200

    changes

--- main.js
+++ main.js
@@ -1,12 +1,48 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-   
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+const initSlider = () => {
+    console.log('SWIPER');
+
+    return new Swiper('.swiper', {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        direction: "horizontal",
+        speed: 400,
+        grabCursor: true,
+        slidesPerGroup: 1,
+
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            900: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            1200: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+                pagination: false,
+            },
+            1400: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+                pagination: false,
+            },
+        },
+    });
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    initSlider();
+
+});
+
